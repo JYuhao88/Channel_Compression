9ddee070e58ac3df602e511b9e63cdb09b0f059a
jyh764790374 <764790374@qq.com>
2021-02-23 13:24:40 +0800

    mask

--- test/test.py
+++ test/test.py
@@ -17,13 +17,24 @@ import math
 
 import torchsnooper
 
-# x = torch.rand(128, 2, 24, 16)
-# r = torch.sqrt(x[:,0,:,:]**2+x[:,1,:,:]**2).unsqueeze(1)
-# theta = torch.arctan(x[:,0,:,:]/x[:,1,:,:]).unsqueeze(1)
-# x = torch.cat((x, r, theta), 1)
-# print(x.size())
 
-x = torch.rand(128, 2, 24, 16)
-y = torch.rand(128, 2, 24, 16)
-xy = x*y
-print(xy.size())
+x = torch.rand(3, 2, 4, 16)
+x = x - 0.5
+r = torch.sqrt(x[:,0,:,:]**2+x[:,1,:,:]**2).unsqueeze(1)
+# theta = torch.atan2(x[:,0,:,:],x[:,1,:,:]).unsqueeze(1)
+# x = torch.cat((x, r, theta), 1)
+
+(r_max, _) = torch.max(r, 3)
+r_mask = r_max<0.6
+r_mask = r_mask.int()
+r_mask = r_mask.unsqueeze(3)
+
+mask = r_mask.expand(-1, -1, -1, 4)
+# print(r_mask[1])
+print(r_mask)
+print(mask)
+
+# x = torch.rand(128, 24)
+# x = x.unsqueeze(1)
+# x = x.unsqueeze(3)
+# print(x.size())
